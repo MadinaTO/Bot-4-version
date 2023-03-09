71276f37ac600350014c5870195b7634dd2bbea9
Madina Tolubaeva <121172709+MadinaTO@users.noreply.github.com>
2023-03-09 10:45:33 +0600

    Update fsm_anketa.py

--- handlers/fsm_anketa.py
+++ handlers/fsm_anketa.py
@@ -10,7 +10,6 @@ class FSMAdmin(StatesGroup):
     age = State()
     gender = State()
     region = State()
-    submit = State()
 
 
 async def fsm_start(message: types.Message):
@@ -54,37 +53,13 @@ async def load_gender(message: types.Message, state: FSMContext):
 async def load_region(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data['region'] = message.text
-    await FSMAdmin.next()
-    await message.answer("Is everything right?", reply_markup=client_kb.submit_markup)
-
-
-async def submit(message: types.Message, state: FSMContext):
-    if message.text.lower() == "yeah":
-        await state.finish()
-    elif message.text.lower() == "repeat":
-        await FSMAdmin.name.set()
-        await message.answer("Name?", reply_markup=client_kb.cancel_markup)
-    else:
-        await message.answer('What!?')
-
-
-async def cancel_reg(message: types.Message, state: FSMContext):
-    current_state = await state.get_state()
-    if current_state is not None:
-        await state.finish()
-        await message.answer("Canceled")
 
 
 def register_handlers_anketa(dp: Dispatcher):
-    dp.register_message_handler(cancel_reg, state="*", commands=['cancel'])
-    dp.register_message_handler(cancel_reg,
-                                Text(equals="cancel", ignore_case=True),
-                                state="*")
-
     dp.register_message_handler(fsm_start, commands=['reg'])
     dp.register_message_handler(load_name, state=FSMAdmin.name)
     dp.register_message_handler(load_age, state=FSMAdmin.age)
     dp.register_message_handler(load_gender, state=FSMAdmin.gender)
     dp.register_message_handler(load_region, state=FSMAdmin.region)
-    dp.register_message_handler(submit, state=FSMAdmin.submit)
+
 
